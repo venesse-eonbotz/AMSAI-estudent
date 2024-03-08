02e2cd28424ecd8192864322653fca082c5b9d1a
venesse.eonbotz <160447592+venesse-eonbotz@users.noreply.github.com>
2024-03-08 08:50:15 +0800

    Update urls.py

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,14 +4,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.urls import path, re_path
-from apps.home import views
+from event import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
     # The home page
-    path('', views.index, name='home'),
+    path('', views.dashboard),
 
     # Matches any html file
     # re_path(r'^.*\.*', views.pages, name='pages'),
