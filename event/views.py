d3a532cae168b4c537d07d5da704a7c11316a066
venesse.eonbotz <160447592+venesse-eonbotz@users.noreply.github.com>
2024-04-05 14:16:32 +0800

    Add files via upload

--- event/views.py
+++ event/views.py
@@ -8,21 +8,35 @@ from .models import Events
 
 # Create your views here.
 def event(request):
-    item = Events.objects.all().order_by('-date', 'status')
-    # for item.date in item:
-    #     item.date.datetime().strftime('%Y/%m/%d %H:%M:%S')
+    try:
+        request.session['login_info']
+    except Exception as e:
+        print(e)
+        return redirect('/amsai/login/')
+    item = Events.objects.all().order_by('-date')
     context = {'item': item}
-    # return render(request, 'payment/payment_list.html', context)
     return render(request, 'admin/events.html', {'item': item})
 
 
 def uploadEvent(request):
     if request.method == "POST":
+        try:
+            request.session['login_info']
+        except Exception as e:
+            print(e)
+            return redirect('/amsai/login/')
+
         item = Events()
         item.title = request.POST.get('title')
         item.venue = request.POST.get('venue')
-        date = request.POST.get('date')
-        date_object = datetime.datetime.strptime(date, '%a %b %d %Y %I:%M %p')
+
+        try:
+            date = request.POST.get('date')
+            date_object = datetime.datetime.strptime(date, '%a %b %d %Y %I:%M %p')
+        except Exception as e:
+            print(e)
+            return render(request, 'admin/events_add.html', {'warn': 'Invalid date format.'})
+
         item.date = date_object
         item.manager = request.POST.get('manager')
         item.attendees = request.POST.get('attendees')
@@ -39,6 +53,12 @@ def uploadEvent(request):
 
 def editEvent(request, nid):
     if request.method == 'GET':
+        try:
+            request.session['login_info']
+        except Exception as e:
+            print(e)
+            return redirect('/amsai/login/')
+
         query = Events.objects.get(id=nid)
         if query.image == "":
             query.image = None
@@ -67,13 +87,22 @@ def refreshEvent(request):
     return redirect('/amsai/events/')
 
 
-# @login_required(login_url="/amsai/login/")
 def dashboard(request):
     count = len(Student.objects.all())
-    event = len(Events.objects.all())
+    mcount =len(Student.objects.filter(gender="Male")) + len(Student.objects.filter(gender="M"))
+    mcount = str(format((mcount / count * 100), ".2f")) + '%'
+    fcount = len(Student.objects.filter(gender="Female")) + len(Student.objects.filter(gender="F"))
+    fcount = str(format((fcount / count * 100), ".2f")) + '%'
+    year = datetime.datetime.today().year
+    event = len(Events.objects.filter(date__year=year))
     date = datetime.datetime.now().date()
-    entry = len(Clocking.objects.filter(date=datetime.datetime.now().date()))
+    entry = len(EntryMonitoring.objects.filter(date=datetime.datetime.now().date()))
+    entry_yesterday = len(EntryMonitoring.objects.filter(date=datetime.datetime.now().date() - datetime.timedelta(days=1)))
     prereg = len(StudentPrereg.objects.filter(reg_status="Pending"))
+    male = len(StudentPrereg.objects.filter(reg_status="Pending", gender="Male"))
+    male = str((male/prereg)*100) + '%'
+    female = len(StudentPrereg.objects.filter(reg_status="Pending", gender="Female"))
+    female = str((female / prereg) * 100) + '%'
     student = len(Student.objects.filter(status="Pending")) + len(Student.objects.filter(status__isnull=True))
     parent = len(Parent.objects.filter(mystatus="Pending"))
     mystudent = len(ParentMystudent.objects.filter(status="Pending"))
@@ -81,10 +110,18 @@ def dashboard(request):
     soa = len(Student.objects.all()) - len(File.objects.all())
     sum = prereg + student + parent + mystudent + pay + soa
     item = Events.objects.all()
+
+    try:
+        request.session['login_info']
+    except Exception as e:
+        print(e)
+        return redirect('/amsai/login/')
+
     for obj in item:
         obj.description = obj.description[:250] + '...'
-    event_list = Events.objects.all().order_by('-date', 'status')[:2]
+    event_list = Events.objects.filter(status=1).order_by('-date')
     return render(request, 'home/dashboard.html', {'count': count, 'event': event, 'parent': parent,
                                                    'date': date, 'entry': entry, 'prereg': prereg, 'student': student,
                                                    'mystudent': mystudent, 'pay': pay, 'soa': soa, 'sum': sum,
-                                                   'item': item, 'event_list': event_list})
+                                                   'item': item, 'event_list': event_list, 'male': male, 'female': female,
+                                                   'mcount': mcount, 'fcount': fcount, 'entry_yesterday': entry_yesterday})
