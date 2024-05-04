74de78e1da4ed817c1644d75a300565e043f5bfb
venesse.eonbotz <160447592+venesse-eonbotz@users.noreply.github.com>
2024-05-04 16:03:22 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -94,6 +94,13 @@ DATABASES = {
 database_url = os.environ.get('DATABASE_URL')
 DATABASES['default'] = dj_database_url.parse(database_url)
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
